29d52ef7e5bf70272a23707f4afe103be38a737e
GitHub <noreply@github.com>
2020-01-16 07:32:02 +0000

    Auto deploy from GitHub Actions
    [d3b36ee] Add SSH CA public key

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Vlab 第一代服务器",
+        "excerpt":"第一代 Vlab 远程虚拟桌面平台在 2019 年 6 月由一位学长初步配置好，经过 @iBug 和 @taoky 等人进一步调整并编写前端管理系统后，于 2019 年秋季学期开始小范围测试（正式上线）。 硬件 第一代 Vlab 服务器包含双路 Intel Xeon E5-2630 v4 处理器（共 20 核心、40 线程），安装内存 128 GB（8 x 16 GB DDR4 2400 ECC）。硬盘配置为一块 Intel Optane 905P 480GB（系统盘，swap 和缓存）和四块 HPE 2.4TB SAS 硬盘。 另外该服务器上还有一块 NVIDIA Tesla T4 GPU，原本计划用于加速深度学习等负载，实际上由于承担的课程实验任务类型不同，并未派上用场。 操作系统 搭建这台服务器的学长选择了 Ubuntu...","categories": [],
+        "tags": [],
+        "url": "https://ibugone.com/vlab/gen1/",
+        "teaser":null},{
+        "title": "Vlab 第二代服务器",
+        "excerpt":"第二代 Vlab 服务器共有 8 台，每台服务器包含双路 Intel Xeon Scalable Silver 4110 处理器（共 16 核心、32 线程），安装内存 160 GB（2 x 16 GB + 4 x 32 GB DDR4 2400 ECC）。硬盘配置为两块 HPE SSD 480GB（RAID 1，系统盘，swap 和缓存）。另有一台存储服务器。  ","categories": [],
+        "tags": [],
+        "url": "https://ibugone.com/vlab/gen2/",
+        "teaser":null},{
+        "title": "SSH 证书认证",
+        "excerpt":"公钥：   ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD6KdAJzKLswXyjf4SipNL1dlK1Vq0KNOit/MTDLiamkqvHJDiKceLYmN97y3Chdx8ofXwW6sRUBJRrjmYq6M0JZSGc8roUtUnSai1P5q1kZQ59x1IhsduTg4WENSteSB6vIvpyoSmcIhi3v9UHgUsl4MsnHxffxx5BiyW7UHPY3MzzkRZL96A4QXUOFd9P+NED3zHmEZ9B2Q66+s2ep2FmNralK4XwRaVxBO2r9san8vYU5pH2TzYZLYxNZ/AFX3bLV5M+AmZytaSNLcuIzZHyqbYawvD+Lee00VB9A3JcaqsjDUCtHZ5gQsZMmqw2r7gj9lDqM6Fw6A8y5rWNJP3Q+FOEEYvzGnQ/SnzU0MpMvGpYWrm/uCJ8pFdbYTYkxAJ+VO0lJ5mAIN664cX0DQ3OyGH/xmCNWGGCfGfmvWqwMwD6Kzo06xcqzsoqaMxwgBuVyICE+VvVCf3pcX4HERDrZY0TMjZxaTc8Ws2xQHJbqekv6nIjQWUgH7LIjkYvycQkxXE2dWfDy/c2SRiKWuxW9n8Hymmfp3lbBHzlCa/LtHeuPIzmBUHUoGya0feWFjrbGnKcPs3etNqpvyIGngMaecTAsbrf5v+J1M0VLCfwzwLt13/G1BCb+BK22vYzMTusrR+6A68Fm6OWSFlBYp31uVLxPg0nqtiW8bi1FbD0hQ==  ","categories": [],
+        "tags": [],
+        "url": "https://ibugone.com/vlab/gen2/ssh-ca/",
+        "teaser":null},]
