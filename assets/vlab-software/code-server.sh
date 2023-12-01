6ef3b5130884d6a332e95f0ad580a24d0e1888f2
GitHub <noreply@github.com>
2023-12-01 05:51:41 +0000

    Auto deploy from GitHub Actions build 341

--- assets/vlab-software/code-server.sh
+++ assets/vlab-software/code-server.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+LOCALDIR=/opt/vlab/code-server
+LOCALVERSION="$(jq -r .version "$LOCALDIR/package.json")"
+
 URL="https://github.com/coder/code-server/releases/latest"
 VERSION="$(curl -sI "$URL" | perl -ne '/^location:/ && /v([\d.]*)\s*$/ && print $1')"
+
+if [ "$VERSION" = "$LOCALVERSION" ]; then
+  echo "Already up-to-date."
+  exit 0
+fi
 
 URL="https://github.com/coder/code-server/releases/download/v$VERSION/code-server-$VERSION-linux-amd64.tar.gz"
 BASENAME="$(basename -s .tar.gz "$URL")"
@@ -10,5 +18,5 @@ set -e
 cd /tmp
 wget -O "$BASENAME.tar.gz" "$URL"
 tar zxf "$BASENAME.tar.gz"
-rsync -avAX "$BASENAME"/ /opt/vlab/code-server/
+rsync -avAX "$BASENAME"/ "$LOCALDIR"/
 rm -rf "$BASENAME" "$BASENAME.tar.gz"
